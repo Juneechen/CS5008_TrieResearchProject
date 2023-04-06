fa050ad728bd82e616210a0c95517f6183e313d0
juneechen <juneechen2021@outlook.com>
2023-04-05 21:33:03 -0400

    separate class defenition and implementation

--- Trie.cpp
+++ Trie.cpp
@@ -2,139 +2,103 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include "Trie.h"
+
 using namespace std;
 
-class Trie {
-public:
-    Trie() {
-        root = new TrieNode();
-        totalWords = 0;
+Trie::Trie() {
+    root = new TrieNode();
+    totalWords = 0;
+}
+
+Trie::~Trie() {
+    delete root;
+}
+
+void Trie::insert(string word) {
+    TrieNode* curr = root;
+    for (char c : word) {
+        c = tolower(c);
+        if (curr->children.find(c) == curr->children.end()) {
+            curr->children[c] = new TrieNode();
+        }
+        curr = curr->children[c];
+        
     }
 
-    ~Trie() {
-        delete root;
+    if (!curr->isWord) {
+        totalWords++;
+        curr->isWord = true;
     }
-    
-    void insert(string word) {
-        TrieNode* curr = root;
-        for (char c : word) {
-            c = tolower(c);
-            if (curr->children.find(c) == curr->children.end()) {
-                curr->children[c] = new TrieNode();
-            }
-            curr = curr->children[c];
-            
-        }
+    curr->freq++;
+}
 
-        if (!curr->isWord) {
-            totalWords++;
-            curr->isWord = true;
+bool Trie::search(string word) {
+    TrieNode* curr = root;
+    for (char c : word) {
+        c = tolower(c);
+        if (curr->children.find(c) == curr->children.end()) {
+            return false;
         }
-        curr->freq++;
+        curr = curr->children[c];
     }
-    
-    bool search(string word) {
-        TrieNode* curr = root;
-        for (char c : word) {
-            c = tolower(c);
-            if (curr->children.find(c) == curr->children.end()) {
-                return false;
-            }
-            curr = curr->children[c];
+    return curr->isWord;
+}
+
+bool Trie::startsWith(string prefix) {
+    TrieNode* curr = root;
+    for (char c : prefix) {
+        c = tolower(c);
+        if (curr->children.find(c) == curr->children.end()) {
+            return false;
         }
-        return curr->isWord;
+        curr = curr->children[c];
     }
-    
-    bool startsWith(string prefix) {
-        TrieNode* curr = root;
-        for (char c : prefix) {
-            c = tolower(c);
-            if (curr->children.find(c) == curr->children.end()) {
-                return false;
-            }
-            curr = curr->children[c];
+    return true;
+}
+
+void Trie::print() {
+    cout << "\nTotal words: " << this->totalWords << endl;
+    printNode(this->root, "");
+    cout << "\n";
+}
+
+vector<string> Trie::getAllWithPrefix(string prefix) {
+    vector<string> res;
+    TrieNode* curr = root;
+    for (char c : prefix) {
+        c = tolower(c);
+        if (curr->children.find(c) == curr->children.end()) {
+            return res;
         }
-        return true;
+        curr = curr->children[c];
     }
+    getAllWithPrefixHelper(res, curr, prefix);
+    return res;
+}
 
-    void print() {
-        cout << "\nTotal words: " << this->totalWords << endl;
-        printNode(this->root, "");
-        cout << "\n";
+Trie::TrieNode::TrieNode() : isWord(false), freq(0), children(10) {}
+
+Trie::TrieNode::~TrieNode() {
+    for (auto& each : this->children) {
+        delete each.second;
     }
+}
 
-    vector<string> getAllWithPrefix(string prefix) {
-        vector<string> res;
-        TrieNode* curr = root;
-        for (char c : prefix) {
-            c = tolower(c);
-            if (curr->children.find(c) == curr->children.end()) {
-                return res;
-            }
-            curr = curr->children[c];
-        }
-        getAllWithPrefixHelper(res, curr, prefix);
-        return res;
+void Trie::printNode(TrieNode * curr, string prefix) {
+    if (curr->isWord) {
+        cout << prefix << "\tfrequency: " << curr->freq << endl;
     }
-
-private:
-    struct TrieNode {
-        bool isWord;
-        int freq;
-        unordered_map<char, TrieNode*> children;
-
-        TrieNode() : isWord(false), freq(0), children(10) {}
-
-        ~TrieNode() {
-            for (auto& each : this->children) {
-                delete each.second;
-            }
-        }
-    };
-
-    TrieNode* root;
-    int totalWords;
-
-    void printNode(TrieNode * curr, string prefix) {
-        if (curr->isWord) {
-            cout << prefix << "\tfrequency: " << curr->freq << endl;
-        }
-        for (auto each : curr->children) {
-            printNode(each.second, prefix + each.first);
-        }
+    for (auto each : curr->children) {
+        printNode(each.second, prefix + each.first);
     }
+}
 
-    void getAllWithPrefixHelper(vector<string>& res, TrieNode* curr, string prefix) {
-        if (curr->isWord) {
-            res.push_back(prefix);
-        }
-        for (auto each : curr->children) {
-            getAllWithPrefixHelper(res, each.second, prefix + each.first);
-        }
+void Trie::getAllWithPrefixHelper(vector<string>& res, TrieNode* curr, string prefix) {
+    if (curr->isWord) {
+        res.push_back(prefix);
     }
-};
-
-int main() {
-    Trie myTrie;
-
-    myTrie.insert("apply");
-    myTrie.insert("cat");
-    myTrie.insert("apple");
-    myTrie.insert("apply");
-    myTrie.insert("catalog");
-    myTrie.insert("banana");
-    myTrie.insert("keep");
-    myTrie.insert("bad");
-
-    myTrie.print();
-
-    vector<string> words = myTrie.getAllWithPrefix("a");
-
-    cout << words.size();
-
-    for (string each : words) {
-        cout << each << endl;
-    } 
-
-    return 0;
+    for (auto each : curr->children) {
+        getAllWithPrefixHelper(res, each.second, prefix + each.first);
+    }
 }
---
+++ Trie.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Trie {
+public:
+    Trie();
+    ~Trie();
+
+    void insert(string word);
+    bool search(string word);
+    bool startsWith(string prefix);
+    void print();
+    vector<string> getAllWithPrefix(string prefix);
+
+private:
+    struct TrieNode {
+        bool isWord;
+        int freq;
+        unordered_map<char, TrieNode*> children;
+
+        TrieNode();
+        ~TrieNode();
+    };
+
+    TrieNode* root;
+    int totalWords;
+
+    void printNode(TrieNode* curr, string prefix);
+    void getAllWithPrefixHelper(vector<string>& res, TrieNode* curr, string prefix);
+};
+
